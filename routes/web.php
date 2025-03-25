6cc8b6cc8c1794ff0c58339139046adc8e10f8e1
miguelba18 <miguel_bahamonro@fet.edu.co>
2025-03-25 17:08:03 -0500

    world

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return '<h1>Hello World!</h1><p>Backend is connected successfully.</p><p>Current time: ' . now()->toDateTimeString() . '</p>';
 });
